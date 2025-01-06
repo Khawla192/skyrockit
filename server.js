2306d2f36d05a3a938c2835370c96938d511c202
Khawla192 <202005171@stu.uob.edu.bh>
2025-01-06 20:39:29 +0300

    Skyrockit application pages and controllers and routes

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
+require('./config/database')
+
 const express = require('express');
+const morgan = require('morgan');
+const session = require('express-session');
+
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const morgan = require('morgan');
-const session = require('express-session');
+
+// require the middleware!
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+const applicationsController = require('./controllers/applications.js');
 
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
-mongoose.connect(process.env.MONGODB_URI);
-
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
-
+// MIDDLEWARE
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // app.use(morgan('dev'));
@@ -28,22 +30,24 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
+app.use(passUserToView);
 
-app.get('/vip-lounge', (req, res) => {
+//ROUTES
+// PUBLIC ROUTES
+app.get('/', (req, res) => {
   if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
+    res.redirect(`/users/${req.session.user._id}/applications`);
   } else {
-    res.send('Sorry, no guests allowed.');
+    res.render('index.ejs');
   }
 });
 
+// PROTECTED ROUTES
 app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users/:userId/applications', applicationsController); 
 
+// LISTENER
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
