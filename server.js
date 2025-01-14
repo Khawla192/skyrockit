b24480af53e489dc295bae0bffea6a4c6624ec05
Khawla192 <202005171@stu.uob.edu.bh>
2025-01-14 13:47:48 +0300

    delete the tag in new.ejs

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const methodOverride = require('method-override');
 // require the middleware!
 const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
-const applicationsController = require('./controllers/applications.js');
 
+const applicationsController = require('./controllers/applications.js');
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
